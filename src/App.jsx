d0d68f9fadcf34a0ee705626e7ae0f148ae0efbb
Fatima Ezzahra <felemenoun@gmail.com>
2023-01-11 18:02:57 +0100

    Read basic test with  Web Speech API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import './App.css'
+import React, { useState, useRef } from 'react';
 
 function App() {
+  const textRef = useRef();
+ const [text, setText] = useState('test');
+  const [highlightedWord, setHighlightedWord] = useState('');
+  const words = text.split(' ');
+
+  const textHandler = (e) => {
+    e.preventDefault();
+    setText(textRef.current.value);
+    console.log(text);
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'fr';
+    utterance.onboundary = (event) => {
+      setHighlightedWord(event.char);
+    };
+
+    synth.speak(utterance);
+    console.log(highlightedWord);
+  }
 
   return (
-    <div className="App">
-      app
-    </div>
-  )
+   <>
+   <form onSubmit={(e) => textHandler(e)}>
+    <textarea ref={textRef} />
+    <button type="submit">Submit</button>
+   </form>
+     <div>
+       {words.map((word, index) => (
+         <span key={index} className={word === highlightedWord ? 'highlighted' : ''}>
+           {word} {' '}
+         </span>
+       ))}
+     </div>
+   </>
+  );
 }
 
 export default App
